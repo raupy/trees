b4632893425b583e57a262d927683fd6ea3ec402
raupy <lilli.schmidt@fu-berlin.de>
2020-06-12 18:51:48 +0200

    adds loading hint

--- app/index.js
+++ app/index.js
@@ -5,37 +5,21 @@ import { gettext } from "i18n";
 const title = document.getElementById("title");
 title.text = gettext("Planted trees:");
 var count = document.getElementById("treecount");
-test();
+count.text = gettext("Loading...");
+load();
 
 function Sleep(milliseconds) {
  return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-async function test() {
- console.log("Vor der sleep-Funktion");
-  if(count.text === ""){
-   console.log("empty string");
- }
- else{
-   console.log(count.text);
- }
- await Sleep(3000); // Pausiert die Funktion für 3 Sekunden
- console.log("Nach der Sleep Funktion");
- if(count.text === ""){
+async function load() {
+ await Sleep(3500); // waiting for connection
+ if(count.text === gettext("Loading...")){
    count.text = gettext("No connection");
  }
- else{
-   console.log(count.text);
- }
 }
-
 
 // Listen for the onmessage event
 messaging.peerSocket.onmessage = function(evt) {
   count.text = (parseInt(evt.data)).toLocaleString();
 }
-
-
-
-
-
